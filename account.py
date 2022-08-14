13e4ab64e07de9178a0456b0fbf4b6811926b996
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-14 19:23:25 -0300

    encapsulating

--- account.py
+++ account.py
@@ -1,6 +1,23 @@
 class Account:
     def __init__(self, number, owner, balance, limit):
-        self.number = number
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        self.__balance -= amount
+
+    def statement(self):
+        print("Account balance is: {}".format(self.__balance))
+        
+    def transfer(self, value, destination):
+        if(self.__balance - value > -(self.__limit) ):
+            self.withdraw(value)
+            destination.deposit(value)
+            print("Your transaction was successful! The value of {} was sent to {}".format(value, destination.__owner))
+        else:
+            print("Sorry, the transaction could not be completed! Make sure you have the limit to do this transfer.")
